02abc40997c2ab5bba8740da25805a2c61816bd2
ChaboCode <kaerdos.dev@protonmail.com>
2025-08-13 20:35:10 -0600

    Typescript compilation patches

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }, [translateMenu])
 
   function Pic({ src, enabled, setPic }: { src: string; enabled?: boolean; setPic: () => void }) {
-    return <img src={src} alt={''} onClick={e => setPic()} className={`min-w-[300px] rounded-xl object-cover ${enabled ? 'opacity-100 shadow-xl' : 'opacity-50 scale-80'} hover:opacity-100 transition duration-300 hover:cursor-pointer `} width={300} />
+    return <Image src={src} alt={''} onClick={() => setPic()} className={`min-w-[300px] rounded-xl object-cover ${enabled ? 'opacity-100 shadow-xl' : 'opacity-50 scale-80'} hover:opacity-100 transition duration-300 hover:cursor-pointer `} width={300} />
   }
 
   const pics = [
@@ -46,17 +46,17 @@ export default function Home() {
       </header>
       <div className="flex items-stretch gap-6">
         <nav className={`relative transition-all border border-amber-500 w-[300px] rounded hover:shadow-md shadow-amber-100 overflow-clip hover:scale-105 duration-200 ease-in-out`}>
-          <div className={`absolute w-[300px] h-[60px] bg-amber-100  -z-30 transition translate-menu`} style={{ '--translate-menu': translateMenu }}></div>
+          <div className={`absolute w-[300px] h-[60px] bg-amber-100  -z-30 transition translate-menu`} style={{ '--translate-menu': translateMenu } as React.CSSProperties}></div>
           <ul className="grid grid-rows-1">
-            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(0)} onMouseEnter={e => setElement(0)}>Elemento 1</li>
-            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(1)} onMouseEnter={e => setElement(1)}>Elemento 2</li>
-            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(2)} onMouseEnter={e => setElement(2)}>Elemento 3</li>
-            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(3)} onMouseEnter={e => setElement(3)}>Elemento 4</li>
-            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(4)} onMouseEnter={e => setElement(4)}>Elemento 5</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={() => setElement(0)} onMouseEnter={() => setElement(0)}>Elemento 1</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={() => setElement(1)} onMouseEnter={() => setElement(1)}>Elemento 2</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={() => setElement(2)} onMouseEnter={() => setElement(2)}>Elemento 3</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={() => setElement(3)} onMouseEnter={() => setElement(3)}>Elemento 4</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={() => setElement(4)} onMouseEnter={() => setElement(4)}>Elemento 5</li>
           </ul>
         </nav>
         <main className="w-[800px] rounded flex flex-col items-center overflow-hidden mx-auto inset-shadow-2xs inset-shadow-white gap-6">
-          <div className={`flex h-[300px] gap-[32px] m-[30px] ${translate} transition duration-300 translatable`} style={{ '--translate': translate }}>
+          <div className={`flex h-[300px] gap-[32px] m-[30px] ${translate} transition duration-300 translatable`} style={{ '--translate': translate } as React.CSSProperties}>
             {pics.map((src, index) =>
               <Pic key={'pic' + index} src={src} enabled={pic === index} setPic={() => setPic(index)} />
             )}
@@ -65,7 +65,7 @@ export default function Home() {
         </main>
       </div>
       <footer className="relative border border-lime-500 rounded h-[120px] flex items-center justify-center group overflow-clip">
-        <div className="absolute -translate-y-5 group-hover:translate-y-0 top-0  transition bg-amber-500 w-[20px] h-[20px] flex items-center justify-center rounded-b text-white hover:cursor-pointer shadow-none group-hover:shadow-amber-300 group-hover:shadow hover:shadow-md right-[30px]" onClick={e => setFooter(!footer)}>?</div>
+        <div className="absolute -translate-y-5 group-hover:translate-y-0 top-0  transition bg-amber-500 w-[20px] h-[20px] flex items-center justify-center rounded-b text-white hover:cursor-pointer shadow-none group-hover:shadow-amber-300 group-hover:shadow hover:shadow-md right-[30px]" onClick={() => setFooter(!footer)}>?</div>
         <span className={`${footer ? 'opacity-100' : 'opacity-0'} transition`}>a poco si jala</span>
       </footer>
     </div>
--- components/pics.tsx
+++ components/pics.tsx
@@ -6,9 +6,9 @@ export default function Pics() {
     return (
 
         <div className="flex w-full gap-4 items-center justify-center h-[100vh]">
-            <button onClick={e => setPic(0)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 0 ? '100' : '60'} hover:cursor-pointer hover:h-[500px]`} src={'/1.jpg'} alt={""} width={400} height={300} /></button>
-            <button onClick={e => setPic(1)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 1 ? '100' : '60'} hover:cursor-pointer`} src={'/2.jpeg'} alt={""} width={400} height={300} /></button>
-            <button onClick={e => setPic(2)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 2 ? '100' : '60'} hover:cursor-pointer`} src={'/3.jpeg'} alt={""} width={400} height={300} /></button>
+            <button onClick={() => setPic(0)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 0 ? '100' : '60'} hover:cursor-pointer hover:h-[500px]`} src={'/1.jpg'} alt={""} width={400} height={300} /></button>
+            <button onClick={() => setPic(1)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 1 ? '100' : '60'} hover:cursor-pointer`} src={'/2.jpeg'} alt={""} width={400} height={300} /></button>
+            <button onClick={() => setPic(2)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 2 ? '100' : '60'} hover:cursor-pointer`} src={'/3.jpeg'} alt={""} width={400} height={300} /></button>
         </div>
     )
 }
