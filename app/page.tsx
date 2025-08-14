a2faa571a3079558a64fa7b97249308a441c342f
ChaboCode <kaerdos.dev@protonmail.com>
2025-08-13 20:20:45 -0600

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,72 @@
+'use client'
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { useEffect, useState } from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+export default function Home() {
+  const [el, setElement] = useState(0)
+
+  const [pic, setPic] = useState(0)
+  const [translate, setTranslate] = useState('0')
+
+  const [translateMenu, setTranslateMenu] = useState('0px')
+
+  const [footer, setFooter] = useState(false)
+
+  useEffect(() => {
+    setTranslateMenu(el * 60 + 'px')
+  }, [el])
+
+  useEffect(() => {
+    setTranslate(((-332 * (pic - 3))) - 150 + 'px')
+  }, [pic])
+
+  useEffect(() => {
+    console.log(translateMenu)
+  }, [translateMenu])
+
+  function Pic({ src, enabled, setPic }: { src: string; enabled?: boolean; setPic: () => void }) {
+    return <img src={src} alt={''} onClick={e => setPic()} className={`min-w-[300px] rounded-xl object-cover ${enabled ? 'opacity-100 shadow-xl' : 'opacity-50 scale-80'} hover:opacity-100 transition duration-300 hover:cursor-pointer `} width={300} />
+  }
+
+  const pics = [
+    'https://rotoplas.kaerdos.dev/files/2507895430.jpeg',
+    'https://rotoplas.kaerdos.dev/files/2755309932.jpg',
+    'https://rotoplas.kaerdos.dev/files/3779635697.gif',
+    'https://rotoplas.kaerdos.dev/files/22258913.jpg',
+    'https://rotoplas.kaerdos.dev/files/3999752392.jpeg',
+    'https://rotoplas.kaerdos.dev/files/3246183970.jpeg',
+  ]
+
+  return (
+    <div className="flex flex-col gap-6">
+      <header className="border border-red-500 w-full h-[60px] rounded flex items-center justify-center group">
+        <span className="group-hover:scale-105 transition group-hover:-translate-y-1">Super header</span>
+      </header>
+      <div className="flex items-stretch gap-6">
+        <nav className={`relative transition-all border border-amber-500 w-[300px] rounded hover:shadow-md shadow-amber-100 overflow-clip hover:scale-105 duration-200 ease-in-out`}>
+          <div className={`absolute w-[300px] h-[60px] bg-amber-100  -z-30 transition translate-menu`} style={{ '--translate-menu': translateMenu }}></div>
+          <ul className="grid grid-rows-1">
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(0)} onMouseEnter={e => setElement(0)}>Elemento 1</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(1)} onMouseEnter={e => setElement(1)}>Elemento 2</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(2)} onMouseEnter={e => setElement(2)}>Elemento 3</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(3)} onMouseEnter={e => setElement(3)}>Elemento 4</li>
+            <li className={`h-[60px] p-4 hover:cursor-pointer hover:text-amber-900 transition duration-200`} onClick={e => setElement(4)} onMouseEnter={e => setElement(4)}>Elemento 5</li>
+          </ul>
+        </nav>
+        <main className="w-[800px] rounded flex flex-col items-center overflow-hidden mx-auto inset-shadow-2xs inset-shadow-white gap-6">
+          <div className={`flex h-[300px] gap-[32px] m-[30px] ${translate} transition duration-300 translatable`} style={{ '--translate': translate }}>
+            {pics.map((src, index) =>
+              <Pic key={'pic' + index} src={src} enabled={pic === index} setPic={() => setPic(index)} />
+            )}
+          </div>
+          <a href={pics[pic]} target="_blank" className="mb-6 p-4 rounded-xl bg-blue-600 text-white transition hover:scale-105 hover:-translate-y-2 hover:shadow-blue-300 shadow hover:shadow-lg">Abrir imagen</a>
+        </main>
+      </div>
+      <footer className="relative border border-lime-500 rounded h-[120px] flex items-center justify-center group overflow-clip">
+        <div className="absolute -translate-y-5 group-hover:translate-y-0 top-0  transition bg-amber-500 w-[20px] h-[20px] flex items-center justify-center rounded-b text-white hover:cursor-pointer shadow-none group-hover:shadow-amber-300 group-hover:shadow hover:shadow-md right-[30px]" onClick={e => setFooter(!footer)}>?</div>
+        <span className={`${footer ? 'opacity-100' : 'opacity-0'} transition`}>a poco si jala</span>
       </footer>
     </div>
   );
---
+++ components/pics.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Pics() {
+    const [pic, setPic] = useState(1)
+    return (
+
+        <div className="flex w-full gap-4 items-center justify-center h-[100vh]">
+            <button onClick={e => setPic(0)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 0 ? '100' : '60'} hover:cursor-pointer hover:h-[500px]`} src={'/1.jpg'} alt={""} width={400} height={300} /></button>
+            <button onClick={e => setPic(1)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 1 ? '100' : '60'} hover:cursor-pointer`} src={'/2.jpeg'} alt={""} width={400} height={300} /></button>
+            <button onClick={e => setPic(2)}><Image className={`transition object-cover rounded h-[300px] opacity-${pic === 2 ? '100' : '60'} hover:cursor-pointer`} src={'/3.jpeg'} alt={""} width={400} height={300} /></button>
+        </div>
+    )
+}
